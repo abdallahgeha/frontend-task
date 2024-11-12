db106bc0eb6ed1c974139d9648a143612f9341e9
Abdallah Geha <geha.abdallah@gmail.com>
2024-11-12 19:59:18 +0100

    Minimum character limit

--- src/components/autocomplete/Autocomplete.vue
+++ src/components/autocomplete/Autocomplete.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { useSearchStore } from '@/store/searchStore'
 import AutoCompleteInput from './AutoCompleteInput.vue'
+import MinChar from './MinChar.vue'
 import SelectCategory from './SelectCategory.vue'
+import { MIN_CHAR_INPUT } from '@/common/constants'
+
+const searchStore = useSearchStore()
 </script>
 
 <template>
   <SelectCategory />
   <AutoCompleteInput />
+  <MinChar v-if="searchStore.searchParam.length < MIN_CHAR_INPUT" />
 </template>
---
+++ src/components/autocomplete/MinChar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { MIN_CHAR_INPUT } from '@/common/constants'
+</script>
+
+<template>
+  <p class="flex items-center mt-2 p-2 pl-6 text-white">
+    Please type at least {{ MIN_CHAR_INPUT }} characters to search.
+  </p>
+</template>
