2b5000c568e351264265ca6a42e7dccd3703ce44
Abdallah Geha <geha.abdallah@gmail.com>
2024-11-12 19:06:01 +0100

    add title to page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
-    <div>Hello world!</div>
+  <main class="mx-auto p-8 w-full max-w-3xl h-screen">
+    <h1 class="mb-8 text-5xl text-gray-50 md:text-6xl">Autocomplete</h1>
   </main>
 </template>
