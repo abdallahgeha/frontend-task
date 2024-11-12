bde8717fc43e3f756effafdfe0f3db907bd22772
Abdallah Geha <geha.abdallah@gmail.com>
2024-11-12 21:42:17 +0100

    improve typing

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,6 +1,7 @@
 import type { books } from '@/data/books'
 import type { cities } from '@/data/cities'
+import type { SearchCategory } from './constants'
 
 export type BookType = (typeof books)[number]
 export type CityType = (typeof cities)[number]
-export type SearchResult = { title: string; type: string; subtitle?: string }
+export type SearchResult = { title: string; type: SearchCategory; subtitle?: string }
